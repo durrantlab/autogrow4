748b7a4215d70ea4ae6062a857e3eedea77cf359
jspiegel <jspiegel@pitt.edu>
2019-11-19 18:55:35 -0500

    utitilies and installation script

--- utility_scripts/file_concatination_and_compression.py
+++ utility_scripts/file_concatination_and_compression.py
@@ -20,55 +20,79 @@ The formating of the concatination is:
     ... Content of the 2nd file...
     "\n##############################$$END_FILE$$ {}".format(os.path.basename(file_name_2))
 
+Example decompression:
+    python autogrow4/utility_scripts/file_concatination_and_compression.py \
+    --compress_or_decompress decompress \
+    --input_folder_or_file PATH_TO_RUN/Run_0/generation_1/PDBs/compresed_PDBS.txt.gz
+Example compression:
+    python autogrow4/utility_scripts/file_concatination_and_compression.py \
+    --compress_or_decompress compress \
+    --input_folder_or_file PATH_TO_RUN/Run_0/generation_1/PDBs/
+
 This concatinated file is tar.gz compressed.
 """
-
 import __future__
+
+import glob
+import os
+import gzip
+import shutil
+import argparse
 
 import support_scripts.Multiprocess as mp
 
 
-import glob
-import os
-import sys
-import copy
-import gzip
-import shutil
-
 def compress_file(file_name):
     """
     Compress the concatinated file
+
+    Inputs:
+    :param str file_name: the path to the file to compress.
     """
+
     with open(file_name, "r") as f:
         printout = f.read()
-    printout = printout.encode('utf-8')
-    with gzip.open(file_name + ".gz", 'wb') as f:
+    printout = printout.encode("utf-8")
+    with gzip.open(file_name + ".gz", "wb") as f:
         f.write(printout)
+
 
 #######
 def decompress_file(decompressed_file):
     """
-    Decompress a file
+    Decompress a file. Not used in running the program but is the counter of
+    def compress_file(file_name)
+
+    Inputs:
+    :param str decompressed_file: the path to the file to decompress.
+
+    Returns:
+    :returns: str decompressed_file: the path to the file to decompress.
     """
-    out_file = decompressed_file.replace('.gz',"")
-    with gzip.open(decompressed_file, 'rb') as f_comp:
-        with open(out_file, 'wb') as f_decomp:
+    out_file = decompressed_file.replace(".gz", "")
+    with gzip.open(decompressed_file, "rb") as f_comp:
+        with open(out_file, "wb") as f_decomp:
             shutil.copyfileobj(f_comp, f_decomp)
     return out_file
 
+
 #######
-def seperate_files(compressed_file, outfolder):
+def seperate_files(compressed_file):
     """
-    This will seperate a compressed and concatinated file into seperated decompressed files.
+    Seperate a concatinated file. Not used in running the program but is the
+    counter of def compress_file(file_name)
 
+    Inputs:
+    :param str compressed_file: the path to the file to seperate/decompress.
     """
-    directory = os.path.abspath(compressed_file.split(os.path.basename(compressed_file))[0]) + os.sep
 
+    directory = (
+        os.path.abspath(compressed_file.split(os.path.basename(compressed_file))[0])
+        + os.sep
+    )
     compressed_file = os.path.abspath(compressed_file)
-    if compressed_file.split(".")[-1] == "gz":
-        decompressed_file = decompress_file(compressed_file)
-    else:
-        decompressed_file= compressed_file
+
+    decompressed_file = decompress_file(compressed_file)
     if os.path.exists(decompressed_file) is False:
         raise Exception("Failed to decompress the file")
 
@@ -88,16 +112,21 @@ def seperate_files(compressed_file, outfolder):
 
                 printout = ""
 
-                # Split the line up and grab the relative file path
-                # convert to absolute path
-                out_file = outfolder + os.sep + line.split("##############################File_name: ")[1].replace("\n","")
+                # Split the line up and grab the relative file path convert to
+                # absolute path
+                out_file = (
+                    directory
+                    + os.sep
+                    + line.split("##############################File_name: ")[
+                        1
+                    ].replace("\n", "")
+                )
                 out_file = os.path.abspath(out_file)
                 list_of_new_files.append(out_file)
                 continue
             else:
                 printout = printout + line
                 continue
-
 
     all_are_made = True
     for f in list_of_new_files:
@@ -107,48 +136,66 @@ def seperate_files(compressed_file, outfolder):
     if all_are_made is True:
         torun = "rm {}".format(decompressed_file)
         os.system(torun)
+
+
 #######
 def get_file_info(file_name):
-    file_name_insert = "\n##############################File_name: {}\n".format(os.path.basename(file_name))
-    file_termination_insert = "\n##############################$$END_FILE$$ {}".format(os.path.basename(file_name))
-    concat = file_name_insert + open(file_name).read() + file_termination_insert
-    return concat
-#######
-def del_files(file_or_folder):
     """
-    This deletes all temporary files.
+    Used for concatinating files together. This function appends a seperator
+    and the filename of a file before and after the text of the file
+    file_name. It returns it as a string
 
     Inputs:
-    :param str file_or_folder: the file or folder to delete
+    :param str file_name: the path to the file to compress.
 
+    Returns:
+    :returns: str concat: the text of the file file_name with a seperator and
+        label before and after the file text.
     """
-    if os.path.exists(file_or_folder) is True:
-        if os.path.isdir(file_or_folder) is True:
-            try:
-                shutil.rmtree(file_or_folder)
-            except:
-                pass
-        else:
-            try:
-                os.remove(command)
-            except:
-                pass
+    file_name_insert = "\n##############################File_name: {}\n".format(
+        os.path.basename(file_name)
+    )
+    file_termination_insert = "\n##############################$$END_FILE$$ {}".format(
+        os.path.basename(file_name)
+    )
+    concat = file_name_insert + open(file_name).read() + file_termination_insert
+    return concat
 
-        # If it failed to delete try via bash command
-        if os.path.exists(file_or_folder) is True:
-            command = "rm -rf {}".format(file_or_folder)
-            try:
-                os.system(command)
-            except:
-                pass
-    else:
-        pass
-    if os.path.exists(file_or_folder):
-        print("couldn't delete file: {}".format(file_name))
+
+#######
+def del_files(file_name):
+    """
+    This function deletes a given file file_name.
+
+    Inputs:
+    :param str file_name: the path to delete.
+    """
+
+    if os.path.exists(file_name):
+        try:
+            os.system("rm {}".format(file_name))
+        except:
+            print("couldn't delete file: {}".format(file_name))
+
+
 #######
 def run_concatination(directory):
+    """
+    This function concatinates and compresses every file in a directory. This
+    makes data transfer easier later on.
+
+    To decompress the folder please script in
+    $PATH/autogrow/utility_scripts/file_concatination_and_compression.py
+
+    Inputs:
+    :param str directory: the path to the folder which will be compiled and compressed.
+    """
+
     concat_file = directory + os.sep + "compresed_PDBS.txt"
-    print("Start Concatination: To seperate files use the file_concatination_and_compression.py in the Utility script folder.")
+    print(
+        "Start Concatination: To seperate files use the \
+        file_concatination_and_compression.py in the Utility script folder."
+    )
     file_list = glob.glob(directory + os.sep + "*")
     file_list = [os.path.abspath(x) for x in file_list]
 
@@ -156,63 +203,102 @@ def run_concatination(directory):
         for file_name in file_list:
             f.write(get_file_info(file_name))
 
-    job_list = [(file_path,) for file_path in file_list]
+    job_list = tuple([(file_path,) for file_path in file_list])
     print("\tFinish Concatination")
     print("\tRemoving files that were concatinated")
     mp.multi_threading(job_list, -1, del_files)
     print("\tCompressing file")
     compress_file(concat_file)
-    if os.path.exists(concat_file  + ".gz"):
+    if os.path.exists(concat_file + ".gz"):
         del_files(concat_file)
     print("Finished Compression")
-#######
-if __name__ == "__main__":
-    try:
-        todo = sys.argv[1]
-        directory = sys.argv[2]
-    except:
-        print("Use this script to concatinate the files in a folder into a single outputfolder\
-        or deconcatinate files into their original format. This is useful for transfering files\
-        more efficiently. example use:\
-                1) python concatinate_file.py concat $PATH/Dir/ \
-                2) python concatinate_file.py concat $PATH/Dir/")
+######
+def run_main(vars):
+    """
+    This function runs the functions for compression or decompression.
+    Inputs:
+    :param dict vars: dictionary of user variables.
+    """
 
-    directory = os.path.abspath(directory)
-    lig_list = glob.glob(directory +os.sep+ "*")
-    lig_list = [os.path.abspath(x) for x in lig_list]
-
-    print("BEFORE")
-    print(os.path.getsize(directory))
-
-
-    concat_file = directory + os.sep + "PDB_concated.txt"
-
-
-    if sys.argv[1] == "concat":
-        run_concatination(directory)
-        print("FINISH CONCAT")
-        print("After concate")
-        print(os.path.getsize(directory))
-    elif sys.argv[1] == "deconcat":
-        compressed_file = sys.argv[2]
-        if os.path.exists(compressed_file) is False:
-            raise Exception("File to Decompress doesn't exist")
-        try:
-            outfolder = sys.argv[3]
-        except:
-            outfolder = os.path.abspath(compressed_file.split(os.path.basename(compressed_file))[0]) + os.sep
-        if os.path.exists(outfolder) is False:
-            os.mkdir(outfolder)
-
-        directory = os.path.abspath(compressed_file.split(os.path.basename(compressed_file))[0]) + os.sep
+    if vars["compress_or_decompress"] == "compress":
 
         print("BEFORE")
-        print(os.path.getsize(directory))
+        input_folder = vars["input_folder_or_file"]
+        print(os.path.getsize(input_folder))
 
-        seperate_files(compressed_file,outfolder)
+        run_concatination(input_folder)
+        print("FINISH CONCAT")
+        print("After concate")
+        print(os.path.getsize(input_folder))
+
+    elif vars["compress_or_decompress"] == "decompress":
+        compressed_file = vars["input_folder_or_file"]
+
+        if os.path.exists(compressed_file) is False:
+            raise Exception("File to Decompress doesn't exist")
+
+        input_folder = os.path.abspath(compressed_file.split(os.path.basename(compressed_file))[0]) + os.sep
+
+        print("BEFORE")
+        print(os.path.getsize(input_folder))
+
+        seperate_files(compressed_file)
         print("After deconcate")
-        print(os.path.getsize(directory))
+        print(os.path.getsize(input_folder))
 
         del_files(compressed_file)
         print("After deconcate")
-        print(os.path.getsize(directory))
+        print(os.path.getsize(input_folder))
+
+#######
+
+
+
+def get_arguments_from_argparse(arg_dict):
+    """
+    This function handles the arg parser arguments for the script.
+
+    Inputs:
+    :param dict arg_dict: dictionary of parameters
+    Returns:
+    :returns: dict arg_dict: dictionary of parameters
+    """
+
+
+    # Argument handling
+    if type(arg_dict["compress_or_decompress"]) != str:
+        raise Exception("Must chose between compress or decompress")
+    if arg_dict["compress_or_decompress"].lower() not in ["compress", "decompress"]:
+        raise Exception("Must chose between compress or decompress")
+
+    # set to lower case to prevent issues
+    arg_dict["compress_or_decompress"] = arg_dict["compress_or_decompress"].lower()
+
+    #  argument_handling
+    if type(arg_dict["input_folder_or_file"]) is not str:
+        raise Exception("--input_folder_or_file required: Path to directory to \
+                        compress or decompress.")
+    if os.path.exists(arg_dict["input_folder_or_file"]) is False:
+        raise Exception("--input_folder_or_file could not be found: \
+                        {}".format(arg_dict["input_folder_or_file"]))
+
+    # Make sure variable is full path and add os.sep if directory
+    arg_dict["input_folder_or_file"] = os.path.abspath(arg_dict["input_folder_or_file"])
+    if os.path.isdir(arg_dict["input_folder_or_file"]):
+        arg_dict["input_folder_or_file"] = arg_dict["input_folder_or_file"] + os.sep
+
+    return arg_dict
+#
+
+# Argment parsing
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument('--compress_or_decompress', type=str, required=True,
+                    choices=["compress", "decompress"],
+                    help='Chose whether to compress or decompress a directory')
+PARSER.add_argument('--input_folder_or_file', '-i', type=str,
+                    required=True, default=None,
+                    help='Path to directory/file to compress or decompress.')
+
+ARGS_DICT = vars(PARSER.parse_args())
+ARGS_DICT = get_arguments_from_argparse(ARGS_DICT)
+run_main(ARGS_DICT)
