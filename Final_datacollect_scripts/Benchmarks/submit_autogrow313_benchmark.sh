84b659a375849113c58c3bd8b9927a058b9ed31d
jspiegel <jspiegel@pitt.edu>
2020-01-20 19:09:10 -0500

    benchmark 313 tweaks

---
+++ Final_datacollect_scripts/Benchmarks/submit_autogrow313_benchmark.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+#SBATCH --job-name=bench_3v4_smallSMP_Run_0
+#SBATCH --output=/bgfs/jdurrant/jspiegel/Benchmarks/bench_3v4_Run_0.conf.out
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --time=22:59:00
+#SBATCH --cluster=smp
+#SBATCH --partition=high-mem
+#SBATCH --mem=503G
+
+# To ensure we are always running on the 512GB of RAM partition
+# We use the--mem=503G and we also use the free -g command to 
+# create a record of the available RAM.
+free -g
+
+## Define the environment
+#py2.7 settings 
+module purge
+module load mgltools
+module load gcc/8.2.0
+module load python/anaconda2.7-2018.12_westpa
+
+#py3.6 settings 
+# module purge
+# module load mgltools
+# module load gcc/8.2.0
+# module load python/anaconda3.7-2018.12_westpa
+
+highest_folder="/bgfs/jdurrant/jspiegel/Benchmarks/Run_0/"
+mkdir $highest_folder
+
+
+##Pre Run the process to prevent EOF Errors                                     
+python RunAutogrow.py -c
+
+higher_outdir=$highest_folder"313/"
+mkdir $higher_outdir
+
+# 3.1.3 needs the top_ones_to_advance_to_next_generation to seed
+# but for every 1 from advanced then you create and dock 1 less.
+# so 3.1.3 needs to be have a higher mutant/crossover number
+#     so for 3.1.3 there will be 150mut and 150cross with an advance of 100
+#       This will result in 200 docking events per generation
+#       for 4.0.0 the equivalent is 100mut and 100cross with an advance of 0
+#           -this results in 200 docking events per generations.
+# Also generation_num needs to be 1 higher since the last gen is a copy over
+#
+# with timeout of 100seconds ~80% of all dockings complete for Vina Docking
+#       Default setting (which is used in this benchmark) is 200 seconds
+#   
+average_time=0
+for i in 1
+do
+    outfolder=$higher_outdir"Run_$i/"
+    mkdir $outfolder
+    output_txt=$outfolder"test_output.txt"
+    error_txt=$outfolder"test_error.txt"
+    echo "START Autogrow 3.1.3 Run number $i"
+    start_time="$(date +%s%N | cut -b1-13)"
+    date +%s%N | cut -b1-13
+
+    time timeout 86400 /ihome/crc/install/python/anaconda2.7-2018.12_westpa/bin/python /bgfs/jdurrant/jspiegel/old_autogrow/autogrow/autogrow/autogrow_3_1_3.py \
+        --filename_of_receptor /bgfs/jdurrant/jspiegel/autogrow4/tutorial/PARP/4r6eA_PARP1_prepared.pdb \
+        -center_x -70.76 -center_y  21.82 -center_z 28.33 \
+        -size_x 25.0 -size_y 16.0 -size_z 25.0 \
+        -additional_autoclickchem_parameters "+all_reactions" \
+        -allow_modification_without_frag_addition TRUE \
+        -directory_of_source_compounds /bgfs/jdurrant/jspiegel/old_autogrow/autogrow/autogrow/tutorial/starting_compounds/ \
+        -directory_of_fragments /bgfs/jdurrant/jspiegel/old_autogrow/autogrow/autogrow/fragments/MW_150/ \
+        -number_of_mutants_first_generation 50 \
+        -number_of_crossovers_first_generation 50 \
+        -number_of_mutants 85 \
+        -number_of_crossovers 85 \
+        -top_ones_to_advance_to_next_generation 70 \
+        -num_generations 6 -max_seconds_per_generation 18000 \
+        -use_lipinski_filter TRUE -use_strict_lipinski_filter TRUE -use_ghose_filter TRUE \
+        -scoring_function VINA -score_by_ligand_efficiency FALSE -maintain_core FALSE \
+        -minimum_core_atoms_required 4 \
+        -vina_executable /bgfs/jdurrant/jspiegel/autogrow4/autogrow/docking/docking_executables/vina/autodock_vina_1_1_2_linux_x86/bin/vina \
+        -openbabel_bin_directory /ihome/crc/install/python/anaconda2.7-4.2.0/bin/ \
+        -mgltools_directory $MGLTOOLS_HOME/ \
+        -num_processors -1 -output_dir $outfolder \
+        >  $outfolder"test_output.txt" 2>  $outfolder"test_error.txt"
+        exit_status=$?
+    echo ""
+    echo "##############################"
+    echo "EXIT STATUS!!!!!!: "$exit_status
+    echo "##############################"
+    echo ""
+    end_time="$(date +%s%N | cut -b1-13)"
+    echo $end_time "   end"
+    tot_time=$(($end_time-$start_time))
+    echo $tot_time " Total time"
+    tot_time_all=$(($tot_time_all + $tot_time))
+    echo $tot_time_all "Total time ALL"
+
+done
+if [ $exit_status = "124" ]
+then
+    echo "Fail to Finish AUTOGROW 3.1.3"
+    echo "TERMINATE this Run!!!!!!1"
+fi
